2d37ad805eefb6492e840da34dee4f26238c7dcf
Maddie Wagner <musomaddy@gmail.com>
2022-03-18 11:24:51 +1100

    Added documentation comments to song.py:

--- ts_lyric_analysis/song.py
+++ ts_lyric_analysis/song.py
@@ -1,6 +1,19 @@
 from lyrics import Lyrics
 
 class Song:
+    """ Contains all the information needed for the current song.
+
+        Parameters:
+            name (string): the name of the song
+            album (string): the name of the album the song is on.
+            lyrics (Lyrics): the lyrics of the song
+            lyric_source (string): the source where there lyrics is from.
+    
+        Methods:
+            __init__(name, album, lyric_source): creates a new Song object using
+                the given name, album and lyric source. Assumes that the lyrics
+                for the song are saved in an appropriately named file.
+    """
 
     # TODO: write a population script and save all this information in a db so I
     # can look it up easily and not have a bunch of static files floating
@@ -9,6 +22,17 @@ class Song:
     # I will leave the lyrics inside a folder to cut down file size.
 
     def __init__(self, name, album, lyric_source):
+        """ Creates an new song object with the given name album and lyric
+        source.
+        Also populates the lyric data from the file located at:
+            static/lyrics/NAME.txt where NAME is the song name in lower case
+            with punctuation removed.
+        
+        Parameters:
+            name (string): the name of the song
+            album (string): the name of the album that contains the song.
+            lyric_source (string): the source where these lyrics were found.
+        """
         self.name = name
         self.album = album
         self.lyrics = Lyrics(name)
