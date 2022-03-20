6918c7c8ec664258db4009fc7a9abb34b668be61
Maddie Wagner <musomaddy@gmail.com>
2022-03-20 13:35:01 +1100

    Updating documentation inside util

--- ts_lyric_analysis/util.py
+++ ts_lyric_analysis/util.py
@@ -1,4 +1,8 @@
 import string
 
 def remove_punctuation(s):
+    """ Removes all punctuation characters from the given string. This includes
+    not just characters at the end of the string, but any punctuation in the
+    middle as well. """
     return s.translate(str.maketrans({p: None for p in string.punctuation}))
+
