831ae882618e707d6de4f9163019b8c1b9cc610f
Maddie Wagner <musomaddy@gmail.com>
2022-03-22 21:54:59 +1100

    Added all the songs from fearless

--- ts_lyric_analysis/db/create_song_db.py
+++ ts_lyric_analysis/db/create_song_db.py
@@ -20,11 +20,41 @@ def debut_album():
             ("Invisible", album, 13, "LyricFind")
     ]
 
+def fearless_album():
+    album = "Fearless (Taylor's Version)"
+    return [("Fearless", album, 1, "Musixmatch"),
+            ("Fifteen", album, 2, "Musixmatch"),
+            ("Love Story", album, 3, "LyricFind"),
+            ("Hey Stephen", album, 4, "Musixmatch"),
+            ("White Horse", album, 5, "LyricFind"),
+            ("You Belong With Me", album, 6, "Musixmatch"),
+            ("Breathe", album, 7, "Musixmatch"),
+            ("Tell Me Why", album, 8, "LyricFind"),
+            ("You're Not Sorry", album, 9, "Musixmatch"),
+            ("The Way I Loved You", album, 10, "Musixmatch"),
+            ("Forever & Always", album, 11, "LyricFind"),
+            ("The Best Day", album, 12, "LyricFind"),
+            ("Change", album, 13, "Musixmatch"),
+            ("Jump Then Fall", album, 14, "LyricFind"),
+            ("Untouchable", album, 15, "LyricFind"),
+            ("Forever & Always (piano version)", album, 16, "Musixmatch"),
+            ("Come In With The Rain", album, 17, "LyricFind"),
+            ("Superstar", album, 18, "Musixmatch"),
+            ("The Other Side Of The Door", album, 19, "LyricFind"),
+            ("Today Was A Fairytale", album, 20, "LyricFind"),
+            ("You All Over Me", album, 21, "LyricFind", True),
+            ("Mr. Perfectly Fine", album, 22, "LyricFind", True),
+            ("We Were Happy", album, 23, "LyricFind", True),
+            ("That's When", album, 24, "LyricFind", True),
+            ("Don't You", album, 25, "LyricFind", True),
+            ("Bye Bye Baby", album, 26, "Musixmatch", True)]
+
 def populate_db():
     # Making all the songs song objects takes a while. Hopefully when this is
     # deployed I can just do this once at the start and then refer to the
     # existing objects. Creating new 'Song' objects is costly in terms of time.
     da = debut_album()
+    fa = fearless_album()
 
 if __name__ == "__main__":
     populate_db()
