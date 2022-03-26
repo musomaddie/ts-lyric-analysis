d40f80632ab9078fc05ca69386835a62f5c72033
Madeleine Wagner <musomaddy@gmail.com>
2022-03-27 05:16:19 +1100

    Adding tests for the album object when creating the db. Also parameterized the album and song init tests.

--- tests/test_db_init.py
+++ tests/test_db_init.py
@@ -1,3 +1,6 @@
+import pytest
+
+from ts_lyric_analysis.album import Album
 from ts_lyric_analysis.database.store_song_info_in_db import list_debut_album_songs
 from ts_lyric_analysis.database.store_song_info_in_db import list_fearless_album_songs
 from ts_lyric_analysis.db import get_db, init_db
@@ -9,6 +12,7 @@ def _get_songs_from_album(album_name):
     if album_name == "Fearless":
         return list_fearless_album_songs()
     return []
+
 def test_init_db_command(runner, monkeypatch):
     class Recorder(object):
         called = False
@@ -21,18 +25,25 @@ def test_init_db_command(runner, monkeypatch):
     assert "Initialized" in result.output
     assert Recorder.called
 
-def test_init_debut_album(app):
+
+@pytest.mark.parametrize("album_name", [("Taylor Swift"), ("Fearless")])
+def test_init_album_success(app, album_name):
     with app.app_context():
         init_db()
         db = get_db()
-        # Assert the album was added correctly
-        album = db.execute(
-            """SELECT *
-            FROM album_info
-            WHERE album_name = 'Taylor Swift'""").fetchone()
-        assert album is not None
+        album_db = db.execute(
+            """ SELECT * FROM album_info WHERE album_name = ? """,
+            (album_name,)).fetchone()
+        assert album_db is not None
 
-def test_init_debut_album_songs(app):
+        # This check may seem somewhat pointless but it's important to check
+        # that an album object can be created successfully from the information
+        # from the db.
+        album_obj = Album(*album_db[1:])
+        assert album_name == album_obj.name
+
+@pytest.mark.parametrize("album_name", [("Taylor Swift"), ("Fearless")])
+def test_init_songs_success(app, album_name):
     """ Testing this makes the tests take longer, but I believe it is important
     to test this to avoid future issues, and to make the process of writing the
     adding script a bit easier.
@@ -41,64 +52,22 @@ def test_init_debut_album_songs(app):
         init_db()
         db = get_db()
 
-        song_count = 0
-        for song in _get_songs_from_album("Taylor Swift"):
-            song_count += 1
+        song_list = _get_songs_from_album(album_name)
+        assert len(song_list) > 0, f"No songs were found from {album_name}."
+
+        for song in song_list:
             song_db = db.execute(
-                """ SELECT *
-                    FROM song_info
-                    WHERE song_title = ? """, (song[0],)).fetchone()
-            assert song_db is not None
-            song_obj = Song(song_db["song_title"],
-                            "Taylor Swift",
-                            song_db["track_number"],
-                            song_db["lyric_source"],
-                            song_db["is_from_the_vault"])
-            assert song_obj == Song(song[0],
-                                    song[1],
-                                    song[2],
-                                    song[3],
-                                    False)
-        assert song_count != 0
-
-def test_init_fearless_album(app):
-    with app.app_context():
-        init_db()
-        db = get_db()
-        album = db.execute(
-            """ SELECT *
-                FROM album_info
-                WHERE album_name = "Fearless" """
-        ).fetchone()
-
-        assert album is not None
-
-def test_init_fearless_album_songs(app):
-    album_name = "Fearless"
-    with app.app_context():
-        init_db()
-        db = get_db()
-
-        song_count = 0
-        for song in _get_songs_from_album(album_name):
-            song_count += 1
-            song_db = db.execute(
-                """ SELECT *
-                    FROM song_info
-                    WHERE song_title = ? """,
+                """ SELECT * FROM song_info WHERE song_title = ? """,
                 (song[0],)).fetchone()
             assert song_db is not None
 
+            # This may seem a bit pointless, but it's important to check that
+            # the song object can be successfully created. It would be extreme
+            # to check every value as it will be correct as long as the
+            # provided data is.
             song_obj_db = Song(song_db["song_title"],
                                album_name,
                                song_db["track_number"],
                                song_db["lyric_source"],
                                song_db["is_from_the_vault"])
-            song = list(song)
-            song[1] = album_name
-            if len(song) == 4:
-                song.append(False)
-            song_obj_test = Song(*song)
-            assert song_obj_db == song_obj_test
-
-        assert song_count != 0, "No songs were found for the album"
+            assert song_obj_db.name == song[0]
